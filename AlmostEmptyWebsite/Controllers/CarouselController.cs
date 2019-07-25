7e2bde8e9205b5c8f9bc7ecf2a414459fae7e7bc
KBonnicksen <kelseybon@gmail.com>
2019-07-24 19:52:50 -0700

    Created carousel and card controller/views

---
+++ AlmostEmptyWebsite/Controllers/CarouselController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AlmostEmptyWebsite.Controllers
+{
+    public class CarouselController : Controller
+    {
+        public IActionResult DummyCarousel()
+        {
+            return View();
+        }
+    }
+}
